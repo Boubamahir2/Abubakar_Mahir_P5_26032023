13baf1c71136569a37dd42d200c57c4dea5d4e9b
Boubamahir2 <amamahir1@gmail.com>
2023-04-27 00:17:06 +0200

    form selectors added

---
+++ frontend/js/formValidation.js
@@ -0,0 +1,13 @@
+// dom selsectors
+
+const firstNameErrorMsg = document.querySelector("#firstNameErrorMsg");
+const lastNameErrorMsg = document.querySelector("#lastNameErrorMsg");
+const addressErrorMsg = document.querySelector("#addressErrorMsg");
+const cityErrorMsg = document.querySelector("#cityErrorMsg");
+const emailErrorMsg = document.querySelector("#emailErrorMsg");
+
+const firstName = document.getElementById("firstName");
+const lastName = document.getElementById("lastName");
+const address = document.getElementById("address");
+const city = document.getElementById("city");
+const email = document.getElementById("email");
