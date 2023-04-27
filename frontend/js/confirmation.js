26d549eade1c00bf2c3fb30008e7599bf56cff43
Boubamahir2 <amamahir1@gmail.com>
2023-04-27 11:36:26 +0200

    comfirmation message functionality

--- frontend/js/confirmation.js
+++ frontend/js/confirmation.js
@@ -3,3 +3,10 @@ let url = new URL(location.href);
 
 //récupère l'id contenu dans l'url de la page actuelle
 let orderId = url.searchParams.get("orderId"); 
+
+// ajout de l'orderId dans le message de validation //
+const orderIdDOM = document.getElementById("orderId");
+orderIdDOM.innerHTML = `${orderId}`;
+
+////// vide le LocalStorage /////////
+localStorage.clear();
